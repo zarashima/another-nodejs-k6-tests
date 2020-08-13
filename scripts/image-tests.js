d23bf50a2e567a39441962692f6c9f1e27bce74b
Vinh Nguyen <npvinh140589@gmail.com>
2020-08-13 15:16:16 +0700

    Init commits

---
+++ scripts/image-tests.js
@@ -0,0 +1,26 @@
+import http from "k6/http";
+import { check, sleep } from "k6";
+import { Rate } from "k6/metrics"
+
+const errorRate = new Rate('error');
+
+export let options = {
+	vus: 1,
+	duration : '30s',
+	thresholds: {
+		'http_req_duration': ['p(95)<1500'],
+		'error': ["rate<0.1"]
+	}
+}
+
+export default function() {
+	const host = __ENV.HOST_NAME || "another-nodejs-shopping-cart.herokuapp.com"
+	const res = http.get(`https://${host}/`, { "responseType": "text"});
+	const elem = res.html().find('img.img-responsive');
+	const result = check(res, {
+		'status code': r => r.status === 200
+		}
+	);
+	errorRate.add(!result);
+	sleep(1);
+}
